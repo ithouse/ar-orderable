5370ce312262890d98e1292383b115a18d1be26f
Gatis Tomsons <gatis@ithouse.lv>
2010-04-23 09:32:17 +0300

    Added new method 'order_unordered' you should call it the table already has some rows and you want to add orderable column, after this all your old records will be ordered

--- lib/lolita_orderable.rb
+++ lib/lolita_orderable.rb
@@ -11,18 +11,37 @@ module Lolita # :nodoc:
       # @:column [string] column name
       # acts_as_orderable :column => "order_nr"
       def acts_as_orderable options = {}
-        @orderable_column = options[:column] ? options[:column] : "order_nr"
-        @orderable_scope  = options[:scope]
-        self.before_save :pre_save_ordering
-        self.before_destroy :pre_destroy_ordering
-        self.default_scope :order => @orderable_column
-        #self.validates_uniqueness_of @orderable_column, :scope => @orderable_scope if @orderable_scope
-        include Lolita::Orderable::InstanceMethods
+        @orderable_column = options[:column] ? options[:column].to_s : "order_nr"
+        if self.columns_hash.keys.include? @orderable_column
+          @orderable_scope  = options[:scope]
+          self.before_save :pre_save_ordering
+          self.before_destroy :pre_destroy_ordering
+          self.default_scope :order => @orderable_column
+          #self.validates_uniqueness_of @orderable_column, :scope => @orderable_scope
+          include Lolita::Orderable::InstanceMethods
+        else
+          msg = "[IMPORTANT] Lolita Orderable plugin: class #{self} has missing column '#{@orderable_column}'"
+          puts msg if Rails.env == "development"
+          RAILS_DEFAULT_LOGGER.error msg
+        end
       end
 
       # returns options list for :options parameter in Managed config
       def options_for_orderable
-        [["",0]] + (1..self.all_orderable.count).to_a.collect{|i| [i,i]}
+        [["",0]] + (1..self.all.size).to_a.collect{|i| [i,i]}
+      end
+
+      # updates all unordered items puts them into the end of list
+      def order_unordered
+        self.reset_column_information # because before this usual 'add_column' is executed and the new column isn't fetched yet
+        unordered_conditions = ["#{self.orderable_column} IS NULL OR #{self.orderable_column} = 0"]
+        ordered_conditions   = ["#{self.orderable_column} IS NOT NULL AND #{self.orderable_column} != 0"]
+        order_nr = self.count(:conditions => ordered_conditions)
+        items = self.all(:conditions => unordered_conditions)
+        items.each do |item|
+          order_nr += 1
+          self.connection.execute("update #{self.table_name} set #{self.orderable_column} = '#{order_nr}' where id = #{item.id};")
+        end
       end
     end
 
@@ -56,11 +75,12 @@ module Lolita # :nodoc:
         if self.id
           self[self.class.orderable_column] = 1 if self[self.class.orderable_column] == 0
         else
-          self[self.class.orderable_column] = self.all_orderable.count + 1 if self[self.class.orderable_column] == 0
+          self[self.class.orderable_column] = self.all_orderable.size + 1 if self[self.class.orderable_column] == 0
         end
-        all_orderable(["id != ?",self.id]).each do |item|
+        all_orderable(["id != ?",self.id || 0]).each do |item|
+          item[self.class.orderable_column] = 0 if item[self.class.orderable_column].nil?
           if self.id
-            if item[self.class.orderable_column] > self.send("#{self.class.orderable_column}_was")
+            if item[self.class.orderable_column] > (self.send("#{self.class.orderable_column}_was") || 0 )
               if item[self.class.orderable_column] <= self[self.class.orderable_column]
                 item[self.class.orderable_column] -= 1
               end
@@ -69,16 +89,6 @@ module Lolita # :nodoc:
                 item[self.class.orderable_column] += 1
               end
             end
-
-#            if item[self.class.orderable_column] > self[self.class.orderable_column] && item[self.class.orderable_column] < self.send("#{self.class.orderable_column}_was")
-#              item[self.class.orderable_column] += 1
-#            elsif item[self.class.orderable_column] < self.send("#{self.class.orderable_column}_was") && item[self.class.orderable_column] > self[self.class.orderable_column]
-#              item[self.class.orderable_column] -= 1
-#            elsif item[self.class.orderable_column] == self[self.class.orderable_column] && self.send("#{self.class.orderable_column}_was") < self[self.class.orderable_column]
-#              item[self.class.orderable_column] -= 1
-#            elsif item[self.class.orderable_column] == self[self.class.orderable_column] && self.send("#{self.class.orderable_column}_was") > self[self.class.orderable_column]
-#              item[self.class.orderable_column] += 1
-#            end
             
             if item[self.class.orderable_column] != item.send("#{self.class.orderable_column}_was")
               self.class.connection.execute("update #{self.class.table_name} set #{self.class.orderable_column} = '#{item[self.class.orderable_column]}' where id = #{item.id};")
